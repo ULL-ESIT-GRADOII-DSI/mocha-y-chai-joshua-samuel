a9a4d85b7ef2450aacf8b8f0c0bbebf21ecf3232
Samuel Ramos Barroso <samuelrb1@gmail.com>
2016-03-18 11:11:13 +0000

    Añadiendo pruebas del constructor medida

--- assets/js/test_temperatura.js
+++ assets/js/test_temperatura.js
@@ -1,6 +1,22 @@
 var expect = chai.expect;
 
-describe("Temperatura", function() {
+describe("Medida", function() {
     
+    describe("Constructor", function() {
+    it("Debería tipo y valor", function() {
+      var t = new Medida (5, 'M');
+      expect(t.valor).to.equal(5);
+      expect(t.tipo).to.equal("M");
+    });
+    
+    it("Debería crearse con 1 parámetro", function() {
+      var t = new Medida (5);
+      expect(t.valor).to.equal(5);
+    });
+    
+    it("No debería crearse sin parámetros", function() {
+      expect(new Medida ()).to.equal.undefined;
+    });
+  });
     
 });
