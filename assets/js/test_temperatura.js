3afbeeb694f7efd81553c7daefbde4ac6695bd78
joshuape <alu0100818819@ull.edu.es>
2016-03-18 11:29:20 +0000

    Test clase Temperatura

--- assets/js/test_temperatura.js
+++ assets/js/test_temperatura.js
@@ -4,9 +4,9 @@ describe("Medida", function() {
     
     describe("Constructor", function() {
     it("Debería tipo y valor", function() {
-      var t = new Medida (5, 'M');
+      var t = new Medida (5, 'c');
       expect(t.valor).to.equal(5);
-      expect(t.tipo).to.equal("M");
+      expect(t.tipo).to.equal("c");
     });
     
     it("Debería crearse con 1 parámetro", function() {
@@ -16,6 +16,27 @@ describe("Medida", function() {
     
     it("No debería crearse sin parámetros", function() {
       expect(new Medida ()).to.equal.undefined;
+    });
+  });
+    
+});
+
+describe("Temperatura", function() {
+    
+    describe("Constructor", function() {
+    it("Debería tipo y valor", function() {
+      var t = new Temperatura (5,'k');
+      expect(t.valor).to.equal(5);
+      expect(t.tipo).to.equal("k");
+    });
+    
+    it("Debería crearse con 1 parámetro", function() {
+      var t = new Temperatura(5);
+      expect(t.valor).to.equal(5);
+    });
+    
+    it("No debería crearse sin parámetros", function() {
+      expect(new Temperatura()).to.equal.undefined;
     });
   });
     
